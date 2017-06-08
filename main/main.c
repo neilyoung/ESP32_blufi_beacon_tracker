e35b7ecaa5ab0d5c9b8967155049bc5ad12f7c6e
decades <foreverneilyoung@googlemail.com>
2017-06-08 16:33:00 +0200

    latest ESP IDF

--- main/main.c
+++ main/main.c
@@ -110,7 +110,7 @@ void bt_task(void *ignore) {
 
     static esp_ble_scan_params_t ble_scan_params = {
         .scan_type = BLE_SCAN_TYPE_PASSIVE,
-        .own_addr_type = ESP_PUBLIC_ADDR,
+        .own_addr_type = BLE_ADDR_TYPE_PUBLIC,
         .scan_filter_policy = BLE_SCAN_FILTER_ALLOW_ALL,
         .scan_interval = 0x50,
         .scan_window =  0x30};
